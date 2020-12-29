584b0c0c4a98c206c7b8ff5f9d487ee017ee81f8
Alex López <alex.lopez.zorzano@gmail.com>
2020-12-29 13:03:06 +0100

    Allow the user to select board size and first peg to remove

--- bin/main.ml
+++ bin/main.ml
@@ -8,8 +8,25 @@ let get_input ?(default="") ()=
 let print_board board = print_string (Pegthing.render_board board)
 
 (** Announce the game is over and prompt to play again *)
-let rec start_game n_rows =
-  prompt_move (Pegthing.new_board n_rows |> Pegthing.remove_peg 4)
+let rec start_game () =
+  let n_rows = prompt_rows () in
+  let board = Pegthing.new_board n_rows |> prompt_empty_peg in
+  prompt_move board
+
+and prompt_empty_peg board =
+  print_string "Here's your board";
+  print_newline ();
+  print_board board;
+  print_string "Remove which peg? [e]";
+  print_newline ();
+  let position = Scanf.sscanf (get_input ~default:"e" ()) "%c"
+                   (fun c -> Pegthing.pos_of_letter c) in
+  Pegthing.remove_peg position board
+
+and prompt_rows () =
+  print_string "How many rows? [5]";
+  print_newline ();
+  Scanf.sscanf (get_input ~default:"5" ()) "%d" Fun.id;
 
 and prompt_move board =
   print_string "\nHere is your board:";
@@ -41,10 +58,10 @@ and game_over board =
   print_string "Play again? y/n [y]";
   print_newline ();
   if (get_input ~default:"y" ()) = "y"
-  then start_game 5
+  then start_game ()
   else print_string "Bye!\n"
 
 
 
 let () =
-  start_game 5
+  start_game ()
